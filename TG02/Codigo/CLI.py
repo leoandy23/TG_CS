c78206ad69181dd55c6adadeaf39c038cbe39ff9
Leonel <leoandy28aa@gmail.com>
2023-07-04 21:20:50 -0500

    Actualizando código

--- TG02/Codigo/CLI.py
+++ TG02/Codigo/CLI.py
@@ -1,6 +1,21 @@
 from Traductor import Traductor
+from colorama import init, Fore
 
-numero = input('Ingresa un número entre 0 y 9,999: ')
-texto_kichwa = Traductor.numero_a_kichwa(numero)
-if (texto_kichwa != None): # Evitar la impresión en caso de que no exista un retorno.
-    print('El número en kichwa es:', texto_kichwa)
+# Inicializar colorama
+init()
+
+print("Bienvenido al traductor de números a kichwa.")
+flag = "s"
+while flag == "s":
+    print(Fore.RESET + "--------------------------------------------------")
+    numero = input(Fore.RESET + "Ingresa un número entre 0 y 9,999: ")
+    texto_kichwa = Traductor.numero_a_kichwa(numero)
+    if (
+        texto_kichwa is not None
+    ):  # Evitar la impresión en caso de que no exista un retorno.
+        print(Fore.GREEN + "El número en kichwa es:", texto_kichwa)
+
+    flag = input(Fore.RESET + "¿Desea continuar? (s/n): ")
+
+print(Fore.RESET + "--------------------------------------------------")
+print(Fore.RESET + "Gracias por usar el traductor de números a kichwa.")
--- TG02/Codigo/Traductor.py
+++ TG02/Codigo/Traductor.py
@@ -1,36 +1,54 @@
-class Traductor():
+from colorama import init, Fore
 
-    # return número en kichwa entre 0 y 9,999
+# Inicializar colorama
+init()
+
+
+class Traductor:
+    # Retorna número en kichwa entre 0 y 9,999
     @staticmethod
     def numero_a_kichwa(numero):
         # Cambio de palabras
-        unidades = ['illak', 'shuk', 'ishkay', 'kimsa', 'chusku', 'pichka', 'sukta', 'kanchis', 'pusak', 'iskun']
-        modificadorPosicion = ['',' chunka',' patsak',' waranka']
+        unidades = [
+            "illak",
+            "shuk",
+            "ishkay",
+            "kimsa",
+            "chusku",
+            "pichka",
+            "sukta",
+            "kanchis",
+            "pusak",
+            "iskun",
+        ]
+        modificador_posicion = ["", " chunka", " patsak", " waranka"]
 
         try:
-            numero_float = float(numero) # Verificación de tipo de dato
-            if (numero_float.is_integer()==False):
+            numero_float = float(numero)  # Verificación de tipo de dato
+            if not numero_float.is_integer():
                 print("El número decimal se trunca")
-            numero_int = int(numero_float) # Conversión a enteros
-            if numero_int == 0:
-                return unidades[0]
-            if numero_int > 0 and numero_int <= 9999: # Verificación de rango
+            numero_int = int(numero_float)  # Conversión a enteros
+            if 0 < numero_int <= 9999:  # Verificación de rango
                 numero_str = str(numero_int)
                 longitud = len(numero_str)
-                resultado = ''
+                resultado = ""
 
                 # Asignación de resultado
-                for i in range (longitud):
-                    if (int(numero_str[i])!=0):
-                        if (i!=0): #Añade una separación si no es el primer número
+                for i in range(longitud):
+                    if int(numero_str[i]) != 0:
+                        if i != 0:  # Añade una separación si no es el primer número
                             resultado += " "
-                        if (int(numero_str[i])==1 and i!=longitud-1): #Controlar 1, 10, 100 y 1000
-                            resultado += modificadorPosicion[longitud-1-i][1:]
+                        if (
+                            int(numero_str[i]) == 1 and i != longitud - 1
+                        ):  # Controlar 1, 10, 100 y 1000
+                            resultado += modificador_posicion[longitud - 1 - i][1:]
                             continue
-                        resultado +=  unidades[int(numero_str[i])] + modificadorPosicion[longitud-1-i]
+                        resultado += (
+                            unidades[int(numero_str[i])]
+                            + modificador_posicion[longitud - 1 - i]
+                        )
                 return resultado
             else:
-                print("Debe ingresar un número entre 0 y 9,999")
+                print(Fore.RED + "Debe ingresar un número entre 0 y 9,999")
         except ValueError:
-            print("El valor ingresado no es un número entero.")
-
+            print(Fore.RED + "El valor ingresado no es un número entero.")
