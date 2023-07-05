8277443c010d0810606c73d2ab0d50600f699550
CesarTufino <cesar.tufino@epn.edu.ec>
2023-07-04 22:57:28 -0500

    Depuracion V2.0

--- TG02/Codigo/CLI.py
+++ TG02/Codigo/CLI.py
@@ -5,17 +5,17 @@ from colorama import init, Fore
 init()
 
 print("Bienvenido al traductor de números a kichwa.")
-flag = "s"
-while flag == "s":
+flag = "S"
+while flag == "S":
     print(Fore.RESET + "--------------------------------------------------")
-    numero = input(Fore.RESET + "Ingresa un número entre 0 y 9,999: ")
+    numero = input(Fore.RESET + "Ingresa un número entre 0 y 9999: ")
     texto_kichwa = Traductor.numero_a_kichwa(numero)
     if (
         texto_kichwa is not None
     ):  # Evitar la impresión en caso de que no exista un retorno.
         print(Fore.GREEN + "El número en kichwa es:", texto_kichwa)
 
-    flag = input(Fore.RESET + "¿Desea continuar? (s/n): ")
+    flag = input(Fore.RESET + "Ingrese \"S\" para volver a usar el traductor: ").upper()
 
 print(Fore.RESET + "--------------------------------------------------")
 print(Fore.RESET + "Gracias por usar el traductor de números a kichwa.")
--- TG02/Codigo/Traductor.py
+++ TG02/Codigo/Traductor.py
@@ -5,7 +5,7 @@ init()
 
 
 class Traductor:
-    # Retorna número en kichwa entre 0 y 9,999
+    # Retorna número en kichwa entre 0 y 9999
     @staticmethod
     def numero_a_kichwa(numero):
         # Cambio de palabras
@@ -24,31 +24,40 @@ class Traductor:
         modificador_posicion = ["", " chunka", " patsak", " waranka"]
 
         try:
-            numero_float = float(numero)  # Verificación de tipo de dato
-            if not numero_float.is_integer():
-                print("El número decimal se trunca")
-            numero_int = int(numero_float)  # Conversión a enteros
-            if 0 < numero_int <= 9999:  # Verificación de rango
-                numero_str = str(numero_int)
-                longitud = len(numero_str)
-                resultado = ""
+            if len(numero) != 0:
+                numero = numero.replace(',', '.')
+                numero_float = float(numero)  # Verificación de tipo de dato
+                if not numero_float.is_integer():
+                    print("El número decimal se trunca")
+                numero_int = int(numero_float)
+                if numero_int == 0 :
+                    return unidades[0]
+                if 0 < numero_int <= 9999:  # Verificación de rango
+                    numero_str = str(numero_int)
+                    longitud = len(numero_str)
+                    resultado = ""
 
-                # Asignación de resultado
-                for i in range(longitud):
-                    if int(numero_str[i]) != 0:
-                        if i != 0:  # Añade una separación si no es el primer número
-                            resultado += " "
-                        if (
-                            int(numero_str[i]) == 1 and i != longitud - 1
-                        ):  # Controlar 1, 10, 100 y 1000
-                            resultado += modificador_posicion[longitud - 1 - i][1:]
-                            continue
-                        resultado += (
-                            unidades[int(numero_str[i])]
-                            + modificador_posicion[longitud - 1 - i]
-                        )
-                return resultado
+                    # Asignación de resultado
+                    for i in range(longitud):
+                        if int(numero_str[i]) != 0:
+                            # Añade una separación si no es el primer número
+                            if i != 0:  
+                                resultado += " "
+                            if (
+                                int(numero_str[i]) == 1 and i != longitud - 1
+                            ):  # Controlar 1, 10, 100 y 1000
+                                resultado = resultado 
+                                + modificador_posicion[longitud - 1 - i][1:]
+                                continue
+                            resultado += (
+                                unidades[int(numero_str[i])]
+                                + modificador_posicion[longitud - 1 - i]
+                            )
+                            
+                    return resultado
+                else:
+                    print(Fore.RED + "Debe ingresar un número entre 0 y 9999")
             else:
-                print(Fore.RED + "Debe ingresar un número entre 0 y 9,999")
+                print(Fore.RED + "No se ha proporcionado ningún valor de entrada")
         except ValueError:
             print(Fore.RED + "El valor ingresado no es un número entero.")
